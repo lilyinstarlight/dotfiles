e29b264c19d76fe9a9f3b74e293b8f7b65ca09de
Foster McLane <fkmclane@gmail.com>
2014-05-26 12:37:06 -0400

    Forgot to remove line

--- .config/qtile/setup.sh
+++ .config/qtile/setup.sh
@@ -2,4 +2,3 @@ xrdb -merge ~/.Xresources
 feh --no-xinerama --bg-scale ~/.background.png
 xsetroot -cursor_name left_ptr
 /usr/libexec/polkit-gnome-authentication-agent-1 &
-chromium-browser --type=service --ignore-gpu-blacklist
