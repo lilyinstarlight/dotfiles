0c7ebaa6473e8ed543e235b92a47a6191dad252b
Lily Foster <lily@lily.flowers>
2020-08-10 08:49:47 -0400

    add more wip install.sh stuff

--- install.sh
+++ install.sh
@@ -1,1 +1,15 @@
 #!/bin/sh -e
+if uname -s | grep -qi 'Linux' &>/dev/null; then
+	os="linux"
+elif uname -s | grep -qi 'Darwin' &>/dev/null; then
+	os="macos"
+elif grep -qi 'Microsoft\|WSL' /proc/version &>/dev/null; then
+	os="wsl"
+else
+	os="common"
+fi
+
+# TODO: commmon overlay
+
+if [ "$os" == "linux" ]; then
+fi
