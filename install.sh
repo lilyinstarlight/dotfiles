b773874e2bca9a2a0113ca3eaf19c07f30ae4c66
Lily Foster <lily@lily.flowers>
2020-08-30 09:27:01 -0400

    make script set its own working directory and add background sync

--- install.sh
+++ install.sh
@@ -1,9 +1,11 @@
 #!/bin/sh -e
-if grep -qi 'Microsoft\|WSL' /proc/version &>/dev/null; then
+cd "$(dirname "$0")"
+
+if grep -qi 'Microsoft\|WSL' /proc/version >/dev/null 2>/dev/null; then
 	os="wsl"
-elif uname -s | grep -qi 'Darwin' &>/dev/null; then
+elif uname -s | grep -qi 'Darwin' >/dev/null 2>/dev/null; then
 	os="macos"
-elif uname -s | grep -qi 'Linux' &>/dev/null; then
+elif uname -s | grep -qi 'Linux' >/dev/null 2>/dev/null; then
 	os="linux"
 else
 	os="unknown"
@@ -13,6 +15,12 @@ echo "Detected OS: $os"
 
 rsync -avv common/ "$HOME"/
 
-if [ "$os" == "linux" ]; then
+if [ "$os" = "linux" ]; then
+	mkdir -p "$HOME"/.backgrounds
+	rsync -avv backgrounds/ "$HOME"/.backgrounds/
+elif [ "$os" = "macos" ]; then
+	mkdir -p "$HOME"/Pictures/Backgrounds
+	rsync -avv backgrounds/1080/ "$HOME"/Pictures/Backgrounds
+elif [ "$os" = "wsl" ]; then
 	true
 fi
