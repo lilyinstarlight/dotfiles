03c69367bda8044c47d6ebcadefd05eae41a8d28
Lily Foster <lily@lily.flowers>
2021-04-28 09:11:16 -0400

    add android detection to install script

--- install.sh
+++ install.sh
@@ -3,9 +3,11 @@ cd "$(dirname "$0")"
 
 if grep -qi 'Microsoft\|WSL' /proc/version >/dev/null 2>/dev/null; then
 	os="wsl"
-elif uname -s | grep -qi 'Darwin' >/dev/null 2>/dev/null; then
+elif uname -s 2>/dev/null | grep -qi 'Darwin' >/dev/null 2>/dev/null; then
 	os="macos"
-elif uname -s | grep -qi 'Linux' >/dev/null 2>/dev/null; then
+elif uname -o 2>/dev/null | grep -qi 'Android' >/dev/null 2>/dev/null; then
+	os="android"
+elif uname -s 2>/dev/null | grep -qi 'Linux' >/dev/null 2>/dev/null; then
 	os="linux"
 else
 	os="unknown"
@@ -29,6 +31,9 @@ elif [ "$os" = 'macos' ]; then
 	mkdir -p "$HOME"/Pictures/Backgrounds
 	rsync -avv backgrounds/1080/ "$HOME"/Pictures/Backgrounds
 elif [ "$os" = 'wsl' ]; then
+	printf 'skipped\n'
+	true
+elif [ "$os" = 'android' ]; then
 	printf 'skipped\n'
 	true
 fi
